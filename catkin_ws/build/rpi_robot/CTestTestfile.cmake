7cd70b9744c75db61397903c11b590cac6c9fdc8
thaije <tjalling_haije@outlook.com>
2017-12-15 19:45:21 +0100

    stuff

--- catkin_ws/build/rpi_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tjalling/Desktop/robot/Robot-v2/catkin_ws/src/rpi_robot
-# Build directory: /home/tjalling/Desktop/robot/Robot-v2/catkin_ws/build/rpi_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
