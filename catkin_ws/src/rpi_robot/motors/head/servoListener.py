c9fc1ac8f89ae8f62de248525705bde574270eb6
Tjalling Haije <tjalling_haije@outlook.com>
2018-03-27 00:03:30 +0200

    primitve face tracking working, needs debugging

--- catkin_ws/src/rpi_robot/motors/head/servoListener.py
+++ catkin_ws/src/rpi_robot/motors/head/servoListener.py
@@ -16,14 +16,14 @@ def cleanup():
 
 def verticalServo(pos):
     rospy.loginfo(rospy.get_caller_id() + ' vertical servo pos %d', pos.data)
-    pos = servoControl[0].getPosition()
-    servoControl[0].setPosition(pos + pos.data)
+    oldPos = servos[0].getPosition()
+    servos[0].setPosition(oldPos + pos.data)
 
 
 def horizontalServo(pos):
     rospy.loginfo(rospy.get_caller_id() + ' horizontal servo pos %d', pos.data)
-    pos = servoControl[1].getPosition()
-    servoControl[1].setPosition(pos + pos.data)
+    oldPos = servos[1].getPosition()
+    servos[1].setPosition(oldPos + pos.data)
 
 
 def listener():
